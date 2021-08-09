ab6bc7e33604171bc8a8a8c3672a5d2704504ddc
nicksin-leroux <68330005+nicksin-leroux@users.noreply.github.com>
2021-08-09 19:44:07 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,12 +1,4 @@
 node default {
-  file {'/root/README':
-    ensure    => file,
-    content   => 'Fok weet',
-    owner     => 'root',
-  } 
-  file {'/root/README':
-  owner    => 'root',
-  } 
 }
 
 
