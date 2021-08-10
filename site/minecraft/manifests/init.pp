52844482d0bb4224ec57c3112c598f5114924298
nicksin-leroux <68330005+nicksin-leroux@users.noreply.github.com>
2021-08-10 14:59:37 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,22 +2,22 @@ class minecraft {
   file {'/opt/minecraft':
     ensure => directory,
   }
-  file {‘/opt/minecraft/server.jar’:
+  file {'/opt/minecraft/server.jar':
     ensure => file,
     source      => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
   }
-  package {‘java’:
+  package {'java':
     ensure => present,
   }
-  file {‘/opt/minecraft/eula.txt’:
+  file {'/opt/minecraft/eula.txt':
     ensure => file,
-    content => ‘eula=true’
+    content => 'eula=true'
   }
-  file {‘/etc/systemd/system/minecraft.service’:
+  file {'/etc/systemd/system/minecraft.service':
     ensure => file,
-    source => ‘puppet:///modules/minecraft/minecraft.service’,
+    source => 'puppet:///modules/minecraft/minecraft.service',
   }
-  service { ‘minecraft’:
+  service { 'minecraft':
     ensure => running,
     enable => true,
   }
